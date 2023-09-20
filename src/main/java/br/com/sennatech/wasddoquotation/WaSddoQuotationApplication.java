f82d2b281efe2108148edfa9443b3a3f1670b941
Bruno Farto <bruno.camargo@sennatech.com.br>
2023-09-20 14:59:14 -0300

    Cotação faltando vincular com o eventhub

---
+++ src/main/java/br/com/sennatech/wasddoquotation/WaSddoQuotationApplication.java
@@ -0,0 +1,13 @@
+package br.com.sennatech.wasddoquotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaSddoQuotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaSddoQuotationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/sennatech/wasddoquotation/service/CalculateQuotation.java
@@ -0,0 +1,23 @@
+package br.com.sennatech.wasddoquotation.service;
+
+import br.com.sennatech.wasddoquotation.domain.dto.QuotationParametersDTO;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Service
+public class CalculateQuotation {
+
+        public BigDecimal quotationCalc(QuotationParametersDTO data) {
+          var coverageSum = data.getCoverages()
+                 .stream()
+                 .map(coverage -> coverage.value())
+                 .reduce(BigDecimal::add).get();
+          if (data.getCoverages().size() > 2){
+              coverageSum = coverageSum.multiply(new BigDecimal(0.8));
+              return coverageSum;
+          }
+          return coverageSum.setScale(2,RoundingMode.HALF_EVEN);
+        }
+}
