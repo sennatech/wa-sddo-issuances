6a19d85015c568b3e0155ae09f31fbed237549fa
Allan Mariano <allanmariano@hotmail.com>
2023-09-21 16:38:16 -0300

    Projeto Inicial

---
+++ src/main/java/br/com/sennatech/wasddoissuances/WaSddoIssuancesApplication.java
@@ -0,0 +1,13 @@
+package br.com.sennatech.wasddoissuances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaSddoIssuancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaSddoIssuancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sennatech/wasddoissuances/WaSddoIssuancesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sennatech.wasddoissuances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaSddoIssuancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
