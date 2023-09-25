61d0ea1bd567b2ba0ec88a897f49290b1e325726
bruno.camargo <bruno.camargo@sennatech.com.br>
2023-09-25 14:38:04 -0300

    arrumando o feign

--- src/main/java/br/com/sennatech/wasddoissuances/service/integration/GetCustumerService.java
+++ src/main/java/br/com/sennatech/wasddoissuances/service/integration/GetCustumerService.java
@@ -1,8 +1,6 @@
 package br.com.sennatech.wasddoissuances.service.integration;
-
-
 import br.com.sennatech.wasddoissuances.domain.Address;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
