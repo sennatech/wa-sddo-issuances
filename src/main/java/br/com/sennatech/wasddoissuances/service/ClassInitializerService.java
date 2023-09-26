d145818f0cb91ddd1313f8ad0e0a32eb6657c113
bruno.camargo <bruno.camargo@sennatech.com.br>
2023-09-26 10:10:02 -0300

    com problemas

--- src/main/java/br/com/sennatech/wasddoissuances/service/ClassInitializerService.java
+++ src/main/java/br/com/sennatech/wasddoissuances/service/ClassInitializerService.java
@@ -1,33 +1,36 @@
-//package br.com.sennatech.wasddoissuances.service;
-//
-//import br.com.sennatech.wasddoissuances.controller.dto.RequestDTO;
-//import br.com.sennatech.wasddoissuances.domain.Coverage;
-//import br.com.sennatech.wasddoissuances.domain.Policy;
-//import br.com.sennatech.wasddoissuances.domain.Validity;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import java.math.BigDecimal;
-//import java.util.BitSet;
-//import java.util.concurrent.ThreadLocalRandom;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class ClassInitializerService {
-//    private  final GetDate getDate;
-//    private final GetTotalValue getTotalValue;
-//    private final CoverageInitializer coverageInitializer;
-//
-//    public Policy policiInitializer(RequestDTO request){
-//        Integer number = ThreadLocalRandom.current().nextInt(1, 9999999 + 1);
-//        Coverage coverage = coverageInitializer.execute(request);
-//        Policy policy = new Policy();
-//        policy.setNumber(number);
-//        policy.setCoverages(request.getCoverages());
-//        BigDecimal totalValue = getTotalValue.execute(request);
-//        Validity validity = new Validity(getDate.getDate(),getDate.calculateDate());
-//
-//
-//    }
-//
-//}
+package br.com.sennatech.wasddoissuances.service;
+
+import br.com.sennatech.wasddoissuances.controller.dto.RequestDTO;
+import br.com.sennatech.wasddoissuances.domain.Coverage;
+import br.com.sennatech.wasddoissuances.domain.Policy;
+import br.com.sennatech.wasddoissuances.domain.Validity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.BitSet;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Service
+@RequiredArgsConstructor
+public class ClassInitializerService {
+    private  final GetDate getDate;
+    private final GetTotalValue getTotalValue;
+
+    public Policy policiInitializer(RequestDTO request){
+        Integer number = ThreadLocalRandom.current().nextInt(1, 9999999 + 1);
+        Policy policy = new Policy();
+        policy.setNumber(number);
+        Validity validity = new Validity(getDate.getDate(),getDate.calculateDate());
+        policy.setValidity(validity);
+        BigDecimal totalValue = getTotalValue.execute(request);
+        policy.setTotalValue(totalValue);
+        policy.setCoverages(request.getCoverages());
+//        policy.setInsuredAdress(request.getInsuredAddress());
+        return policy;
+
+
+
+    }
+
+}
--- src/main/java/br/com/sennatech/wasddoissuances/service/GetTotalValue.java
+++ src/main/java/br/com/sennatech/wasddoissuances/service/GetTotalValue.java
@@ -1,12 +1,13 @@
-//package br.com.sennatech.wasddoissuances.service;
-//
-//import br.com.sennatech.wasddoissuances.controller.dto.RequestDTO;
-//import br.com.sennatech.wasddoissuances.domain.Coverage;
-//
-//import java.math.BigDecimal;
-//
-//public class GetTotalValue {
-//    public BigDecimal execute(RequestDTO requestDTO){
-//        BigDecimal coverageSum = requestDTO.getCoverages().stream().map(coverage -> coverage.getValue()).reduce(BigDecimal::add).get();
-//    }
-//}
+package br.com.sennatech.wasddoissuances.service;
+
+import br.com.sennatech.wasddoissuances.controller.dto.RequestDTO;
+import br.com.sennatech.wasddoissuances.domain.Coverage;
+
+import java.math.BigDecimal;
+
+public class GetTotalValue {
+    public BigDecimal execute(RequestDTO requestDTO){
+        BigDecimal coverageSum = requestDTO.getCoverages().stream().map(coverage -> coverage.getValue()).reduce(BigDecimal::add).get();
+        return  coverageSum;
+    }
+}
